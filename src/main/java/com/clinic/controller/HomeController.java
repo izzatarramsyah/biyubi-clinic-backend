53b06a9be5f016c75c7b4a5e8bca393ae068f998
adl.izzata <adl.izzata@xl.co.id>
2022-05-14 21:34:22 +0700

    make it better

--- src/main/java/com/clinic/controller/HomeController.java
+++ src/main/java/com/clinic/controller/HomeController.java
@@ -12,15 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 	
-	@Value("${message}")
-	private String message;
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		String formattedDate = dateFormat.format(date);
-		return message + " "+formattedDate;
+		return "success connect to config server "+formattedDate;
 	}
 
 }
