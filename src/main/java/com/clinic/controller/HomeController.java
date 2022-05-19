70514bc9673b61e78d33e8cb7194c3862f6eca60
adl.izzata <adl.izzata@xl.co.id>
2022-05-19 16:13:20 +0700

    make it better

--- src/main/java/com/clinic/controller/HomeController.java
+++ src/main/java/com/clinic/controller/HomeController.java
@@ -12,12 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 	
+	@Value("${message}")
+	private String message;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		String formattedDate = dateFormat.format(date);
-		return "success connect to biyubi clinic backend "+formattedDate;
+		return message + " "+ formattedDate;
 	}
 
 }
